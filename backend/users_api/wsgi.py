dad28633ab9e9482ee14f34ac486cd22fae2ae67
Felipe <felipe.bernardi@estudiantes.utec.edu.uy>
2025-10-30 20:55:09 -0300

    Funcionando

--- backend/users_api/wsgi.py
+++ backend/users_api/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Point to the canonical settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users_api.settings_canonical')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users_api.settings')
 
 application = get_wsgi_application()
